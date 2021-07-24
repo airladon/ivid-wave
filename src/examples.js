337c16c7fa16c244b195e97bf2f21702610853bb
airladon <airladon@gmail.com>
2021-07-24 14:52:38 -0700

    update examples

--- src/examples.js
+++ src/examples.js
@@ -4,30 +4,61 @@ function addExamples() {
     location,
     font: { size },
   })
+  const s = 0.8;
   figure.add({
     name: 'examples',
-    make: 'text',
-    font: { color: color1, size: 2 },
-    xAlign: 'center',
-    text: [
-      text('Ocean Waves', 1.2, [18, 9]),
-      text('Sound', 1.2, [12, 2]),
-      text('Light', 1.2, [2.5, 5]),
-      text('Seismic Waves', 0.5, [11, 9.5]),
-      text('Shock Waves', 0.5, [2, 11]),
-      text('Gravity Waves', 0.6, [19, 0.7]),
-      text('Ultrasound', 0.6, [3, 8]),
-      text('Radio', 0.7, [4, 2]),
-      text('Cellular Signals', 0.6, [19, 6]),
-      text('WiFi Signals', 0.8, [21, 4]),
-      text('GPS Signals', 0.6, [8, 3]),
-      text('Water Ripples', 0.5, [7, 1]),
-      text('X-Rays', 0.8, [4, 9]),
-      text('Gamma-Rays', 0.6, [20, 11]),
-      text('Microwaves', 0.5, [8, 10.5]),
-      text('Infrared', 0.7, [14, 1]),
-      text('Vibrating Strings', 0.6, [21, 2.5]),
+    make: 'collection',
+    elements: [
+      {
+        name: 'ocean',
+        make: 'text',
+        font: { color: color1, size: 2 },
+        xAlign: 'center',
+        text: [
+          text('Ocean Waves', 1.2 * s, [18, 9]),
+        ],
+      },
+      {
+        name: 'sound',
+        make: 'text',
+        font: { color: color1, size: 2 },
+        xAlign: 'center',
+        text: [
+          text('Sound', 1.2 * s, [11, 2]),
+        ],
+      },
+      {
+        name: 'light',
+        make: 'text',
+        font: { color: color1, size: 2 },
+        xAlign: 'center',
+        text: [
+          text('Light', 1.2 * s, [2.5, 5]),
+          text('WiFi Signals', 1.2 * s, [21, 4.5]),
+        ],
+      },
+      {
+        name: 'allTheRest',
+        make: 'text',
+        font: { color: color1, size: 2 },
+        xAlign: 'center',
+        text: [
+          text('Seismic Waves', 0.6 * s, [11, 9.5]),
+          text('Shock Waves', 0.6 * s, [2, 11]),
+          text('Gravity Waves', 0.6 * s, [19, 0.7]),
+          text('Ultrasound', 0.6 * s, [3.5, 7]),
+          text('Radio', 0.6 * s, [3, 2]),
+          text('Cellular Signals', 0.6 * s, [20, 7]),
+          text('GPS Signals', 0.6 * s, [6, 3.3]),
+          text('Water Ripples', 0.6 * s, [6, 1]),
+          text('X-Rays', 0.6 * s, [4, 9]),
+          text('Gamma-Rays', 0.6 * s, [21, 11]),
+          text('Microwaves', 0.6 * s, [8, 10.5]),
+          text('Infrared', 0.6 * s, [15, 1]),
+          text('Vibrating Strings', 0.6 * s, [20, 2.5]),
+        ],
+        color: color1,
+      }
     ],
-    color: color1,
   });
 }
