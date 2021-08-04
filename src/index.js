6b9a8d12cc2d0cb64080830cf7dec59cb9760b15
airladon <airladon@gmail.com>
2021-08-04 09:21:37 -0700

    cleanup timeWave colors

--- src/index.js
+++ src/index.js
@@ -391,10 +391,10 @@ figure.shortcuts = {
 // figure.addFrameRate(10, { font: { color: [1, 0, 0, 1 ]} });
 time.setTimeSpeed(1);
 nav.loadSlides([
-  // {
-  //   scenario: 'default',
-  //   show: 'eqnTimeWave',
-  // },
+  {
+    scenario: 'default',
+    show: 'eqnTimeWave',
+  },
   {
     scenario: 'default',
     show: 'eqnSinT',
